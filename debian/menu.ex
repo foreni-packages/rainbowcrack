96aacfc05c999a452ecb145a23c35176fdd85494
Devon Kearns <dookie@kali.org>
2013-01-21 08:23:28 -0700

    Pre-build commit

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(rainbowcrack):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="rainbowcrack" command="/usr/bin/rainbowcrack"
--- debian/rainbowcrack.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the rainbowcrack package
-#
-0 4	* * *	root	[ -x /usr/bin/rainbowcrack_maintenance ] && /usr/bin/rainbowcrack_maintenance
